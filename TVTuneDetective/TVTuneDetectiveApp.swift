2e6e12f8c65db5a1312e018fb1ab64516f825587
Kenneth Riendeau <kriendeau67@gmail.com>
2025-09-26 17:13:56 -0400

    Initial Commit

---
+++ TVTuneDetective/TVTuneDetectiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  TVTuneDetectiveApp.swift
+//  TVTuneDetective
+//
+//  Created by Kenneth Riendeau on 9/26/25.
+//
+
+import SwiftUI
+
+@main
+struct TVTuneDetectiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TVTuneDetectiveTests/TVTuneDetectiveTests.swift
@@ -0,0 +1,17 @@
+//
+//  TVTuneDetectiveTests.swift
+//  TVTuneDetectiveTests
+//
+//  Created by Kenneth Riendeau on 9/26/25.
+//
+
+import Testing
+@testable import TVTuneDetective
+
+struct TVTuneDetectiveTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
